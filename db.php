68614fa623a4d5aac2e957f192970d8a5ce1bbc2
EliChellee <rachelleannemanila04@gmail.com>
2024-05-29 20:48:09 +0800

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $dbname = 'valorantplayersdb';
 $username = 'root';
-$password = 'rachelle040403';
+$password = '';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
